51885291f9df7c3058d4f0bc71a99a1efe7650f8
Michael Wiltfong <wiltfong.michael1@gmail.com>
2022-01-31 21:37:53 -0500

    attached onlick listener to buttons

--- main.js
+++ main.js
@@ -1,5 +1,15 @@
 document.addEventListener("DOMContentLoaded", () => {
   createSquares();
+
+  const keys = document.querySelectorAll(".keyboard-row button");
+  console.log(keys);
+  for (let i = 0; i < keys.length; i++) {
+    keys[i].onclick = ({ target }) => {
+      const letter = target.getAttribute("data-key");
+
+      console.log(letter);
+    };
+  }
   function createSquares() {
     const gameBoard = document.getElementById("board");
 
